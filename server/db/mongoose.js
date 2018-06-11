101f8b092545b99d49ec6efd5ce68bb6474dbf0c
Felipe Gaya Scandiffio <felipe.scandiffio@radixeng.com.br>
2018-06-11 00:03:31 -0300

    commit geral

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://felipegs:159753feh@ds255320.mlab.com:55320/node-course');
 
 module.exports = {
     mongoose    
