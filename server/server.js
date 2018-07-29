c5fcdfe5227a4f5eb674f99813f433b686a4b45b
RADIXENGRJ\felipe.scandiffio <felipe.scandiffio@radixeng.com.br>
2018-07-29 00:01:01 -0300

    +cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+  
 app.post('/cargas', authenticate, (req, res) => {
     var carga = new Carga({
         preco: req.body.preco,
